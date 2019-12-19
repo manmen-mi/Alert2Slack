55243000b304e8b83b0469384815a31fcd913c9b
キム <kim@fb.tapweb.jp>
2019-12-19 15:04:34 +0900

    書き方修正

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const axios = require('axios').default;
+import core from '@actions/core'
+import github from '@actions/github'
+import axios from 'axios'
 
-try {
-  const url = core.getInput('webhook');
-  const title_link = core.getInput('title-link');
-  await axios.post(url, {
-    "attachments": [{
-      "color": "#ff0000",
-      "pretext": "Error in Github actions",
-      "title":"Jump to repository",
-      "title_link": title_link,
-      "fields": [
-        {
-          "title": "Repository",
-          "value": github.context.repo,
-          "short": true
-        },
-        {
-          "title": "Type",
-          "value": "Error",
-          "short": true
-        }
-      ]
-    }]
-  }, {'Content-type':'application/json'});
-  console.log('message posted');
-} catch (error) {
-  core.setFailed(error.message);
+export default async () => {
+  try {
+    const url = core.getInput('webhook')
+    const title_link = core.getInput('title-link')
+    await axios.post(url, {
+      "attachments": [{
+        "color": "#ff0000",
+        "pretext": "Error in Github actions",
+        "title":"Jump to repository",
+        "title_link": title_link,
+        "fields": [
+          {
+            "title": "Repository",
+            "value": github.context.repo,
+            "short": true
+          },
+          {
+            "title": "Type",
+            "value": "Error",
+            "short": true
+          }
+        ]
+      }]
+    }, {'Content-type':'application/json'})
+    console.log('message posted')
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
